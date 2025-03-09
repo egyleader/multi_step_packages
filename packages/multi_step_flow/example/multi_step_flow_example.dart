a7acf35a46d9c3137b855a7728b3d3fd045f0c7b
Ahmed Osama <a.osama@dkilo.com>
2025-03-09 15:16:55 +0200

    init commit

---
+++ packages/multi_step_flow/example/multi_step_flow_example.dart
@@ -0,0 +1,6 @@
+import '../lib/multi_step_flow.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ packages/multi_step_flow/lib/multi_step_flow.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library;
+
+export 'src/multi_step_flow_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
