c5bc81814f8b2c48b8f767958eeba218e2bb9692
Ahmed Osama <a.osama@dkilo.com>
2025-03-11 21:27:53 +0200

    added exports so users don't have to depend on multi_step_flow

--- packages/multi_step_widgets/lib/multi_step_widgets.dart
+++ packages/multi_step_widgets/lib/multi_step_widgets.dart
@@ -17,3 +17,20 @@ export 'src/layouts/flow_layout.dart';
 // Themes
 export 'src/theme/flow_theme.dart';
 export 'src/theme/indicator_theme.dart';
+
+// Re-export types from multi_step_flow
+export 'package:multi_step_flow/multi_step_flow.dart'
+    show
+        // Models
+        FlowStep,
+        FlowConfiguration,
+        FlowStatus,
+        
+        // Controllers
+        FlowController,
+        
+        // BLoC
+        FlowBloc,
+        
+        // Validators
+        FlowValidators;
